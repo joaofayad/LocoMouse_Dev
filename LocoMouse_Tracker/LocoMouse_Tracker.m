970d23922424381593b711bb90a91e14dd656e62
DennisEckmeier <dennis@eckmeier.de>
2016-02-25 14:44:31 +0000

    Figures and tracking coordinates
    - MTF_export_figures() expected constrained coordinates, but received
    unconstrained coordinates. This was fixed.
    - added titles and axis labels to the figures
    - LocoMouse_Tracker() now saves constrained and unconstrained
    coordinates, both

--- LocoMouse_Tracker/LocoMouse_Tracker.m
+++ LocoMouse_Tracker/LocoMouse_Tracker.m
@@ -7,7 +7,7 @@ function varargout = LocoMouse_Tracker(varargin)
 % Author: Joao Fayad (joao.fayad@neuro.fchampalimaud.org)
 % Last Modified: 17/11/2014
 
-% Last Modified by GUIDE v2.5 01-Dec-2015 11:12:18
+% Last Modified by GUIDE v2.5 15-Jan-2016 15:16:59
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -500,17 +500,17 @@ for i_files = 1:Nfiles
                     handles.data.flip = true;
             end
             
-            [final_tracks,tracks_tail,OcclusionGrid,bounding_box,handles.data,debug] = MTF_rawdata(handles.data, handles.model, handles.BoundingBox_choice.Value);
-            [final_tracks,tracks_tail] = convertTracksToUnconstrainedView(final_tracks,tracks_tail,size(handles.data.ind_warp_mapping),handles.data.ind_warp_mapping,handles.data.flip,handles.data.scale);
+            [final_tracks_c,tracks_tail_c,OcclusionGrid,bounding_box,handles.data,debug] = MTF_rawdata(handles.data, handles.model, handles.BoundingBox_choice.Value);
+            [final_tracks,tracks_tail] = convertTracksToUnconstrainedView(final_tracks_c,tracks_tail_c,size(handles.data.ind_warp_mapping),handles.data.ind_warp_mapping,handles.data.flip,handles.data.scale);
             % clearing the background image to avoid problems:
             
             % Saving tracking data:
             data = handles.data;
-            save(data_file_name,'final_tracks','tracks_tail','OcclusionGrid','bounding_box','debug','data');
+            save(data_file_name,'final_tracks','tracks_tail','final_tracks_c','tracks_tail_c','OcclusionGrid','bounding_box','debug','data');
             
             % Saving data plot figures
             if handles.checkbox_ExpFigures.Value
-                MTF_export_figures(final_tracks, tracks_tail, data_file_name);
+                MTF_export_figures(final_tracks_c, tracks_tail_c, data_file_name, data);
             end
             
             % FIXME Performing swing and stance detection:
@@ -644,47 +644,6 @@ function edit_output_path_CreateFcn(hObject, eventdata, handles)
 if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor','white');
 end
-
-% --- Function that loads a model file and performs a few basic checks:
-function handles = loadModel(full_file_path, handles)
-    model = load(full_file_path);
-    % Since there is no model file type we must check we have all the right
-    % fields:
-    if isfield(model,'model')
-        model =model.model;
-    end
-    ModelFieldNames      = fieldnames(model);
-    ExpectedModel        = [{'line'}  {'tail'} ; ...
-                            {'point'} {'paw'} ; ...
-                            {'point'} {'snout'}];
-                        
-    failed = false;
-    if ~any(ismember(ModelFieldNames,'line')) || ~any(ismember(ModelFieldNames,'point'))    
-        for emt = 1:size(ExpectedModel,1)
-             if any(ismember(ModelFieldNames,ExpectedModel(emt,2)))
-                if any(ismember(fieldnames(eval(['model.' char(ExpectedModel(emt,2))])),'w')) && any(ismember(fieldnames(eval(['model.' char(ExpectedModel(emt,2))])),'rho'))
-                     eval(['model.',char(ExpectedModel(emt,1)),'.',char(ExpectedModel(emt,2)),' = model.',char(ExpectedModel(emt,2)),';']);
-                else
-                     failed = true;
-                end
-             else
-                 failed = true;
-             end
-
-        end
-    end
-    if failed
-        error('LocoMouse_Tracker() / loadModel() :: Model file useless.')
-    else
-        if ~isfield(model.point.paw,'N_points')
-            model.point.paw.N_points =4;
-        end
-        if ~isfield(model.point.snout,'N_points')
-            model.point.snout.N_points =1;
-        end
-        handles.model =model;
-    end
-
 
 % --- Function that loads a calibration file and performs a few basic checks:
 function handles = loadCalibrationFile(full_file_path, handles)
