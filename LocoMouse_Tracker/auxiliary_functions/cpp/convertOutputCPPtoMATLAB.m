c602bcde592da89a6c9453dad9b4c5872bf45a1e
joaofayad <joaofayad@gmail.com>
2017-04-20 16:50:16 +0100

    Automated job submission to openlava cluster

--- LocoMouse_Tracker/auxiliary_functions/cpp/convertOutputCPPtoMATLAB.m
+++ LocoMouse_Tracker/auxiliary_functions/cpp/convertOutputCPPtoMATLAB.m
@@ -27,21 +27,24 @@ for i_files = 1:N_files
     
     output_file = fullfile(output_path, ['output_' file_name '.yml']);
     
-    output = readOpenCVYAML(output_file);
-    %     delete(output_file);
-    final_tracks_c = permute(cat(3,output.paw_tracks0,output.paw_tracks1,output.paw_tracks2,output.paw_tracks3,output.snout_tracks0),[2 3 1]);
-    final_tracks_c(final_tracks_c(:)<0) = NaN;
-    final_tracks_c = final_tracks_c + 1;
-    if isfield(output,'tracks_tail')
-        tracks_tail_c = reshape(output.tracks_tail,3,15,[]);
-        tracks_tail_c(tracks_tail_c<0) = NaN;
-        tracks_tail_c = tracks_tail_c + 1;
-    else
-        tracks_tail_c = NaN(3,1,size(final_tracks_c,3));
-    end
+    [final_tracks, tracks_tail, debug] = cppToMATLABTracks(output_file,data);
     
-    [final_tracks,tracks_tail] = convertTracksToUnconstrainedView(final_tracks_c,tracks_tail_c,size(data.ind_warp_mapping),data.ind_warp_mapping,data.flip,data.scale);
-    debug = [];  
+%     output = readOpenCVYAML(output_file);
+%    
+%     %     delete(output_file);
+%     final_tracks_c = permute(cat(3,output.paw_tracks0,output.paw_tracks1,output.paw_tracks2,output.paw_tracks3,output.snout_tracks0),[2 3 1]);
+%     final_tracks_c(final_tracks_c(:)<0) = NaN;
+%     final_tracks_c = final_tracks_c + 1;
+%     if isfield(output,'tracks_tail')
+%         tracks_tail_c = reshape(output.tracks_tail,3,15,[]);
+%         tracks_tail_c(tracks_tail_c<0) = NaN;
+%         tracks_tail_c = tracks_tail_c + 1;
+%     else
+%         tracks_tail_c = NaN(3,1,size(final_tracks_c,3));
+%     end
+%    
+%     [final_tracks,tracks_tail] = convertTracksToUnconstrainedView(final_tracks_c,tracks_tail_c,size(data.ind_warp_mapping),data.ind_warp_mapping,data.flip,data.scale);
+%     debug = [];  
     % Saving tracking data:
     save(fullfile(output_path,[file_name '.mat']),'final_tracks','tracks_tail','final_tracks_c','tracks_tail_c','debug','data');
     
